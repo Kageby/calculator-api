d95cc6319961c8856f98c6964c14a71e447c3e4b
Kageby <dsoegian@purdue.edu>
2026-03-09 16:28:01 +0000

    IA6

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
+from math import sqrt as sqrt
 
 app = FastAPI()
-
 
 @app.get("/", status_code=200)
 def read_root():
@@ -10,7 +10,7 @@ def read_root():
 
 
 @app.get("/add/{a}/{b}", status_code=200)
-def add(a: float, b: float):
+def add(a: str, b: str):
     """
     Add two numbers together.
     
@@ -21,10 +21,16 @@ def add(a: float, b: float):
     Returns:
     - JSON object with the result
     """
+    
+    try: 
+        a = float(a)
+        b = float(b)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
     return {"result": a + b}
 
 @app.get("/subtract/{a}/{b}", status_code=200)
-def subtract(a: float, b: float):
+def subtract(a: str, b: str):
     """
     Subtract two numbers together.
     
@@ -35,4 +41,110 @@ def subtract(a: float, b: float):
     Returns:
     - JSON object with the result
     """
+    try: 
+        a = float(a)
+        b = float(b)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
     return {"result": a - b}
+
+@app.get("/multiply/{a}/{b}", status_code=200)
+def multiply(a: str, b: str):
+    """
+    Multiply two numbers together.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try: 
+        a = float(a)
+        b = float(b)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
+    return {"result": a * b}
+
+@app.get("/divide/{a}/{b}", status_code=200)
+def divide(a: str, b: str):
+    """
+    Divide two numbers.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result or an error if dividing by zero.
+    """
+    try: 
+        a = float(a)
+        b = float(b)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
+    
+    try:
+        if (b == 0):
+            raise ZeroDivisionError
+    except ZeroDivisionError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Division by zero is not allowed. Please provide a non-zero value for b.")
+    return {"result": a / b}
+
+@app.get("/pow/{a}/{b}", status_code=200)
+def pow(a: str, b: str):
+    """
+    a to the power of b.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try: 
+        a = float(a)
+        b = float(b)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
+    return {"result": a ** b}
+
+@app.get("/sqrt/{a}", status_code=200)
+def square_root(a: str):
+    """
+    Square root of a.
+    
+    Parameter:
+    - a: First number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try: 
+        a = float(a)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
+    return {"result": sqrt(a)}
+
+@app.get("/avg/{a}/{b}/{c}", status_code=200)
+def average(a: str, b: str, c: str):
+    """
+    a to the power of b.
+    
+    Parameters:
+    - a: First number
+    - b: Second number
+    - c: Third number
+    
+    Returns:
+    - JSON object with the result
+    """
+    try: 
+        a = float(a)
+        b = float(b)
+        c = float(c)
+    except ValueError: 
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="All arguments must be valid numbers.")
+    return {"result": (sum([a, b, c]) / 3)}
